1d633b7556c75d7b7b867617f41ca36a31b9b859
zaidmukaddam <zaidaltafmukaddam@gmail.com>
2024-09-05 16:51:54 +0530

    feat: Add security headers to all routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'strict-origin-when-cross-origin',
+                    },
+                ],
+            },
+        ]
+    },
     images: {
         dangerouslyAllowSVG: true,
         remotePatterns: [
